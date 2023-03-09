d138145ac62b498a0b1b321009dec3f07db0632b
Lucas Pina <ti.lucaspina@gmail.com>
2023-03-09 11:55:19 -0300

    inicio projetos

--- dashboard/src/views/Home/indexHome.vue
+++ dashboard/src/views/Home/indexHome.vue
@@ -1,8 +1,33 @@
 <template>
-  <h1>Aqui é a home</h1>
+  <custom-Header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact-Home />
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feddback © 2023</p>
+  </footer>
 </template>
-<script>
-export default {
-  
+
+
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+import CustomHeader from './CustomHeader.vue';
+import ContactHome from './ContactHome.vue';
+
+
+onMounted(() => {
+  const token = window.localStorage.getItem('token')
+
+  if (token) { }
+}),
+
+  function handleLogin() {
+
+  }
+
+function handleAccountCreate() {
+
 }
+
 </script>
