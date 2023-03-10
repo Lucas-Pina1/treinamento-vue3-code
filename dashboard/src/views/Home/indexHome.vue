67d2121feda29db8259a74f9734d4751e28b1b02
Lucas Pina <ti.lucaspina@gmail.com>
2023-03-09 21:49:06 -0300

    continuação home

--- dashboard/src/views/Home/indexHome.vue
+++ dashboard/src/views/Home/indexHome.vue
@@ -6,28 +6,23 @@
   </footer>
 </template>
 
-
-
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 
+import CustomHeader from "./CustomHeader.vue";
+import ContactHome from "./ContactHome.vue";
 
-import CustomHeader from './CustomHeader.vue';
-import ContactHome from './ContactHome.vue';
-
+const router = useRouter();
 
 onMounted(() => {
-  const token = window.localStorage.getItem('token')
+  const token = window.localStorage.getItem("token");
 
-  if (token) { }
-}),
-
-  function handleLogin() {
-
+  if (token) {
+    router.push({ name: "Feedbacks" });
   }
+}),
+  function handleLogin() { };
 
-function handleAccountCreate() {
-
-}
-
+function handleAccountCreate() { }
 </script>
