43d2b4af445678db63cc9975330f6ebcaf22db2e
Lucas Pina <ti.lucaspina@gmail.com>
2023-03-16 21:05:15 -0300

    pegando usuario logado

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,5 +4,27 @@
 </template>
 
 <script setup>
+import { watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import ModalFactory from "./components/ModalFactory/Index.vue";
+import services from "./services";
+
+const router = useRouter();
+const route = useRoute();
+
+watch(
+  () => route.path,
+  async () => {
+    if (route.meta.hasAuth) {
+      const token = window.localStorage.getItem("token");
+      if (!token) {
+        router.push({ name: "Home" });
+        return;
+      }
+
+      const { data } = await services.users.getMe();
+      console.log("Data", data);
+    }
+  }
+);
 </script>
