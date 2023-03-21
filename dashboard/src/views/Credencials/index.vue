1faac042ec1680aad439af0c7a674c6ec8382cf2
Lucas Pina <ti.lucaspina@gmail.com>
2023-03-20 21:17:13 -0300

    Credenciais partes 1

---
+++ dashboard/src/views/Credencials/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <HeaderLogged />
+  </div>
+
+  <div class="flex flex-col itens-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Credenciais
+    </h1>
+    <p class="text-lg text-center justify-center h-64 bg-brand-gray">
+      Guia de instalação e geração de suas credenciais
+    </p>
+  </div>
+</template>
+
+<script setup>
+import HeaderLogged from "../../components";
+</script>
