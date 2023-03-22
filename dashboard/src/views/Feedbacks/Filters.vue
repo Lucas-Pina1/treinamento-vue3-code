49ac150802d79110d8864387ba9c0f5f305d7d9f
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-03-22 11:26:10 -0300

    Tela feedbacks parte 1 e 2

---
+++ dashboard/src/views/Feedbacks/Filters.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="flex flex-col">
+    <h1 class="text-2xl font-regular text-brand-darkgray">Filtros</h1>
+
+    <ul class="flex flex-col mt-3 list-none">
+      <li
+        v-for="filter in state.filters"
+        :key="filter.label"
+        :class="{ 'bg-gray-200 bg-opacity-50': filter.active }"
+        class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
+        @click="() => handleSelect(filter)"
+      >
+        <div class="flex items-center">
+          <span
+            :class="`bg-${filter.color}`"
+            class="inline-block w-2 h- mr-2 rounded-full"
+          >
+            {{ filter.label }}
+          </span>
+        </div>
+        <span
+          :class="
+            filter.active ? `text=${filter.color}` : 'text-brand-graydark'
+          "
+          class="font-bold"
+        >
+          {{ filter.amount }}
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script async setup>
+import { reactive } from "vue";
+import services from "../../services/index";
+import useStore from "../../hooks/useStore";
+import { emit } from "process";
+
+const LABELS = {
+  all: "Todos",
+  issue: "Programas",
+  idea: "Ideas",
+  other: "Outros",
+};
+
+const COLORS = {
+  all: "brand-info",
+  issue: "brand-danger",
+  idea: "brand-warning",
+  other: "brand-graydark",
+};
+
+const store = useStore("Global");
+
+const state = reactive({
+  hasError: false,
+  filters: [{ label: null, amount: null }],
+});
+
+function handleSelect({ type }) {
+  if (store.isLoading) {
+    return;
+  }
+
+  state.filters = state.filters.map((filter) => {
+    if (filter.type === type) {
+      return { ...filter, active: true };
+    }
+    return { ...filter, active: false };
+  });
+
+  emit("select", type);
+}
+
+function applyFiltersStructure(summary) {
+  return Object.keys(summary).reduce((acc, cur) => {
+    const currentFilter = {
+      label: LABELS[cur],
+      color: COLORS[cur],
+      amount: summary[cur],
+    };
+
+    if (cur === "all") {
+      currentFilter.active = true;
+    } else {
+      currentFilter.type = cur;
+    }
+    return [...acc, currentFilter];
+  }, []);
+}
+
+try {
+  const { data } = await services.feedbacks.getSummary();
+  state.filters = applyFiltersStructure(data);
+} catch (error) {
+  state.hasError = !!error;
+  state.filters = applyFiltersStructure({ all: 0, issue: 0, idea: 0 });
+}
+</script>
--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -1,8 +1,33 @@
 <template>
-  <h1>feedbacks</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <HeaderLogged />
+  </div>
+
+  <div class="flex flex-col itens-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">Feddbacks</h1>
+    <p class="text-lg text-center justify-center h-64 bg-brand-gray">
+      Detalhes de todos os feedbacks recebidos.
+    </p>
+  </div>
+
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-10 grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black text-brand-darkgray">Listagem</h1>
+        <suspense>
+          <template #default>
+            <Filters />
+          </template>
+          <template #fallback> ...loading </template>
+        </suspense>
+      </div>
+
+      <div class="px-10 pt-20 col-span-3"></div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+import HeaderLogged from "../../components/HeaderLogged/index";
+import Filters from "./Filters.vue";
 </script>
-
